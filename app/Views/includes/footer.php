50f656f9048233a0ce28b4c8a6c7040f007df3e3
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2024-02-06 00:23:22 +0530

    first_commit

--- app/Views/includes/footer.php
+++ app/Views/includes/footer.php
@@ -1,12 +1,12 @@
-<script src="./public/templates/dash-libs/vendor/jquery/jquery.min.js"></script>
-<script src="./public/templates/dash-libs/vendor/jqueryui/js/jquery-ui.min.js"></script>
-<script src="./public/templates/dash-libs/vendor/global/global.min.js"></script>
-<script src="./public/templates/dash-libs/js/custom.min.js"></script>
-<script src="./public/templates/dash-libs/js/quixnav-init.js"></script>
-<script src="./public/templates/dash-libs/vendor/datatables/js/jquery.dataTables.min.js"></script>
-<script src="./public/templates/dash-libs/js/plugins-init/datatables.init.js"></script>
-<script src="./public/templates/dash-libs/vendor/select2/js/select2.full.min.js"></script>
-<script src="./public/templates/dash-libs/js/plugins-init/select2-init.js"></script>
+<script src="../public/templates/dash-libs/vendor/jquery/jquery.min.js"></script>
+<script src="../public/templates/dash-libs/vendor/jqueryui/js/jquery-ui.min.js"></script>
+<script src="../public/templates/dash-libs/vendor/global/global.min.js"></script>
+<script src="../public/templates/dash-libs/js/custom.min.js"></script>
+<script src="../public/templates/dash-libs/js/quixnav-init.js"></script>
+<script src="../public/templates/dash-libs/vendor/datatables/js/jquery.dataTables.min.js"></script>
+<script src="../public/templates/dash-libs/js/plugins-init/datatables.init.js"></script>
+<script src="../public/templates/dash-libs/vendor/select2/js/select2.full.min.js"></script>
+<script src="../public/templates/dash-libs/js/plugins-init/select2-init.js"></script>
 
 </div>
 
