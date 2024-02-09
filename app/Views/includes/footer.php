7abfaf99bb8039319f4fdf0b6208f21499fd3708
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2024-02-09 23:51:45 +0530

    changes

--- app/Views/includes/footer.php
+++ app/Views/includes/footer.php
@@ -2,7 +2,7 @@
 
 echo script_tag('public/templates/dash-libs/vendor/jquery/jquery.min.js');
 ?>
-<script src="/public/templates/dash-libs/vendor/jquery/jquery.min.js"></script>
+<script src="<?php echo site_url('/public/templates/dash-libs/vendor/jquery/jquery.min.js'); ?>"></script>
 <script src="/public/templates/dash-libs/vendor/jqueryui/js/jquery-ui.min.js"></script>
 <script src="/public/templates/dash-libs/vendor/global/global.min.js"></script>
 <script src="/public/templates/dash-libs/js/custom.min.js"></script>
