3cbd6aadb20400e189198e6794c4e4880080375d
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2024-02-10 18:08:47 +0530

    changes

--- app/Views/includes/footer.php
+++ app/Views/includes/footer.php
@@ -1,6 +1,6 @@
 <?php
 
-echo script_tag('public/templates/vendor/jquery/jquery.min.js');
+echo script_tag('public/assets/vendor/jquery/jquery.min.js');
 ?>
 <!-- <script src="public/templates/vendor/jqueryui/js/jquery-ui.min.js"></script>
 <script src="public/templates/vendor/global/global.min.js"></script>
