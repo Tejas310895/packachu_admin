95d0aa3ad1af8149ddd0f36d6bc9996df0329884
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2024-02-10 18:02:25 +0530

    changes

--- app/Views/includes/footer.php
+++ app/Views/includes/footer.php
@@ -1,16 +1,15 @@
 <?php
 
-echo script_tag('public/templates/dash-libs/vendor/jquery/jquery.min.js');
+echo script_tag('public/templates/vendor/jquery/jquery.min.js');
 ?>
-<script src="<?php echo site_url('/public/templates/dash-libs/vendor/jquery/jquery.min.js'); ?>"></script>
-<script src="public/templates/vendor/jqueryui/js/jquery-ui.min.js"></script>
+<!-- <script src="public/templates/vendor/jqueryui/js/jquery-ui.min.js"></script>
 <script src="public/templates/vendor/global/global.min.js"></script>
 <script src="public/templates/js/custom.min.js"></script>
 <script src="public/templates/js/quixnav-init.js"></script>
 <script src="public/templates/vendor/datatables/js/jquery.dataTables.min.js"></script>
 <script src="public/templates/js/plugins-init/datatables.init.js"></script>
 <script src="public/templates/vendor/select2/js/select2.full.min.js"></script>
-<script src="public/templates/js/plugins-init/select2-init.js"></script>
+<script src="public/templates/js/plugins-init/select2-init.js"></script>  -->
 
 </div>
 
