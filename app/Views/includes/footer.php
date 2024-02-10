aca96ddbc462a24675c3505bf1c01adb462a99d1
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2024-02-10 18:42:40 +0530

    changes

--- app/Views/includes/footer.php
+++ app/Views/includes/footer.php
@@ -1,6 +1,20 @@
 <?php
 
-echo script_tag('public/assets/vendor/jquery/jquery.min.js');
+$assets_arr = [
+    "public/assets/vendor/jquery/jquery.min.js",
+    "public/assets/vendor/jqueryui/js/jquery-ui.min.js",
+    "public/assets/vendor/global/global.min.js",
+    "public/assets/js/custom.min.js",
+    "public/assets/js/quixnav-init.js",
+    "public/assets/vendor/datatables/js/jquery.dataTables.min.js",
+    "public/assets/js/plugins-init/datatables.init.js",
+    "public/assets/vendor/select2/js/select2.full.min.js",
+    "public/assets/js/plugins-init/select2-init.js"
+];
+
+foreach ($assets_arr as $assets) {
+    echo script_tag($assets);
+}
 ?>
 <!-- <script src="public/templates/vendor/jqueryui/js/jquery-ui.min.js"></script>
 <script src="public/templates/vendor/global/global.min.js"></script>
